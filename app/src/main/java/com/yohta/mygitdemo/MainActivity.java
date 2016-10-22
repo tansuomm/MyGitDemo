4008974156032232e8860f25e671157d032045af
yuanbao <1309877947@qq.com>
2016-10-22 15:53:24 +0800

    添加全屏

--- app/src/main/java/com/yohta/mygitdemo/MainActivity.java
+++ app/src/main/java/com/yohta/mygitdemo/MainActivity.java
@@ -2,11 +2,15 @@ package com.yohta.mygitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
